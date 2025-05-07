21d949c2acb80ef42078988c1eea256f8d3ef9e6
mks257 <138339735+mks257@users.noreply.github.com>
2025-05-07 12:32:13 -0500

    added all js files

--- classes/Manager.js
+++ classes/Manager.js
@@ -1,10 +1,25 @@
-class Manager {
-
-
-
-    
+// manager.js
+const { Employee } = require('./employee');
+class Manager extends Employee {
+    #employeesManaged;
+    constructor(name, position, salary, department) {
+        super(name, position, salary);
+        this.department = department;
+        this.#employeesManaged = [];
+    }
+    getEmployeesManaged() {
+        return this.#employeesManaged;
+    }
+    addEmployeeManaged(employee) {
+        this.#employeesManaged.push(employee);
+    }
+    promote(newPosition, raiseAmount) {
+        // Optionally include extra promotion logic for managers
+        console.log(`${this.name} has been promoted to ${newPosition} with a raise of $${raiseAmount}`);
+        super.promote(newPosition, raiseAmount);
+    }
 }
 
 module.exports = {
     Manager,
-}
+};
--- classes/SalesPerson.js
+++ classes/SalesPerson.js
@@ -1,10 +1,32 @@
-class SalesPerson {
+// salesperson.js
 
+const { Employee } = require('./employee');
 
+class SalesPerson extends Employee {
+    #totalSales;
 
-    
+    constructor(name, position, salary, clients) {
+        super(name, position, salary);
+        this.clients = clients; // Public array
+        this.#totalSales = 0;   // Private field
+    }
+
+    getSalesNumbers() {
+        return this.#totalSales;
+    }
+
+    makeSale(amount) {
+        if (amount < 0) {
+            throw new Error("Sale amount cannot be negative");
+        }
+        this.#totalSales += amount;
+    }
+
+    findClient(clientName) {
+        return this.clients.find(c => c.toLowerCase() === clientName.toLowerCase()) || null;
+    }
 }
 
 module.exports = {
     SalesPerson,
-}
+};
